6c2661c6614a384caa47e7c8e3fe7cc983df6f0c
Lundjo <lukadjelic529@gmail.com>
2026-05-08 14:19:43 +0200

    feat: configure edge to edge and navigation bar styling

--- app/src/main/java/com/lundjo/museapptimer/MainActivity.kt
+++ app/src/main/java/com/lundjo/museapptimer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.lundjo.museapptimer
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.lundjo.museapptimer.navigation.AppNavigation
@@ -10,7 +11,9 @@ import com.lundjo.museapptimer.ui.theme.MuseAppTimerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.dark(android.graphics.Color.TRANSPARENT)
+        )
         setContent {
             MuseAppTimerTheme {
                 AppNavigation()
