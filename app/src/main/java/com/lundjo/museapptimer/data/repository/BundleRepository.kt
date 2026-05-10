d4f4a0e157976bbe31fb0412ef59763f4cc78efa
Lundjo <lukadjelic529@gmail.com>
2026-05-10 15:33:32 +0200

    feat: add BundleRepository

--- app/src/main/java/com/lundjo/museapptimer/data/repository/BundleRepository.kt
+++ app/src/main/java/com/lundjo/museapptimer/data/repository/BundleRepository.kt
@@ -1,2 +1,20 @@
 package com.lundjo.museapptimer.data.repository
 
+import com.lundjo.museapptimer.data.db.AppDatabase
+import com.lundjo.museapptimer.data.model.Bundle
+import kotlinx.coroutines.flow.Flow
+
+class BundleRepository(private val database: AppDatabase) {
+
+    fun getAllBundles(): Flow<List<Bundle>> {
+        return database.bundleDao().getAll()
+    }
+
+    suspend fun insertBundle(bundle: Bundle) {
+        database.bundleDao().insert(bundle)
+    }
+
+    suspend fun deleteBundle(bundle: Bundle) {
+        database.bundleDao().delete(bundle)
+    }
+}
